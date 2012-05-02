2ace40f7fb8400e328d6546ebc37a28c022735cf
ceejaay <chad.jemmett@gmail.com>
2012-05-01 20:47:38 -0600

    worked on this before using github.

--- mod_roman_numbers1.rb
+++ mod_roman_numbers1.rb
@@ -1,24 +1,27 @@
-#completed on unknown date.
 #roman numerals 2 3rd attempt.
 
 def roman_numeral year
-  thou = year/1000
-  thou_remain = year%1000
-  five_hundreds = thou_remain/500
-  hundreds = (thou_remain%500)/100
-  fifties = ((thou_remain%500)%100)/50
-  tens = (((thou_remain%500)%100)%50)/10
-  fives = ((((thou_remain%500)%100)%50)%10)/5
-  ones = (((((thou_remain%500)%100)%50)%10)%5)/1
+  thou = year/1000 # = M
+  thou_remain = year%1000 
+  five_hundreds = thou_remain/500 # = D
+  hundreds = (thou_remain%500)/100 # = C
+  # =90  XC
+  fifties = ((thou_remain%500)%100)/50 # = L
+  tens = (((thou_remain%500)%100)%50)/10 # = X
+  fives = ((((thou_remain%500)%100)%50)%10)/5 # = V
+  ones = (((((thou_remain%500)%100)%50)%10)%5)/1 # = I
   
   
   #this is just to clear the terminal screen so you only see the result.
 100.times do puts "" 
     end
-  
+
+
   #outputs the letters times the number returned.
   puts "M" * thou + "D" * five_hundreds + "C" * hundreds + "L" * fifties + "X" * tens + "V" * fives + "I" * ones
 end
+
+if
 
 puts "Hi, put in a number and I'll spit out the roman numeral equivilant."
   year = gets.chomp.to_i
--- modern_roman_numerals_1.rb
+++ modern_roman_numerals_1.rb
@@ -1,5 +1,5 @@
-#completed on unknown date.
-#roman numerals 2 3rd attempt.
+#completed on March 27, 2012
+#I recycled a bunch of the old old-style roman numeral code.
 
 def roman_numeral year
   thou = year/1000
@@ -9,19 +9,22 @@ def roman_numeral year
   fifties = ((thou_remain%500)%100)/50
   tens = (((thou_remain%500)%100)%50)/10
   fives = ((((thou_remain%500)%100)%50)%10)/5
-  ones = (((((thou_remain%500)%100)%50)%10)%5)/1
-  
+  fours = (((((thou_remain%500)%100)%50)%10)%5)/4
+  threes = ((((((thou_remain%500)%100)%50)%10)%5)%4)/3
+  ones =  (((((((thou_remain%500)%100)%50)%10)%5)%4)%3)/1
   
   #this is just to clear the terminal screen so you only see the result.
 100.times do puts "" 
     end
   
   #outputs the letters times the number returned.
-  puts "M" * thou + "D" * five_hundreds + "C" * hundreds + "L" * fifties + "X" * tens + "V" * fives + "I" * ones
+  puts "M" * thou + "D" * five_hundreds + "C" * hundreds + "L" * fifties + "X" * tens + "V" * fives + "IV" * fours + "III" * threes + "I" * ones
 end
 
 puts "Hi, put in a number and I'll spit out the roman numeral equivilant."
   year = gets.chomp.to_i
 
+#this is the extreme long equation that makes all this work.
+#(((((((thou_remain%500)%100)%50)%10)%5)%4)%3)/1
 #call the method with the user-given year.
 roman_numeral year
